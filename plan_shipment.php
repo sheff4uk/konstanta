a17951b74985021ea15539db8b98325c1595abeb
sheff4uk <sheff4uk@gmail.com>
2026-05-13 15:44:58 +0300

    fix

--- plan_shipment.php
+++ plan_shipment.php
@@ -229,11 +229,11 @@ while( $row = mysqli_fetch_array($res) ) {
 				}
 				echo "</td>";
 				echo "<td rowspan='{$subcnt}'>";
-				// if( $subrow["prior"] ) {
-				if( $subrow["friendly_shipment_time"] ) {
+				if( $subrow["prior"] ) {
 					echo "<a href='printforms/shipment_blank.php?PS_ID={$subrow["PS_ID"]}' class='print' style='margin-right: 20px;' title='Бланк накладной'><i class='fas fa-print fa-lg'></i></a>";
 				}
-				else {
+				// else {
+				if( !$subrow["friendly_shipment_time"] ) {
 					echo "<a href='#' class='add_ps' ps_id='{$subrow["PS_ID"]}' ps_date='{$row["ps_date"]}' priority='{$subrow["priority"]}' title='Изменить запланированную отгрузку'><i class='fa fa-pencil-alt fa-lg'></i></a>";
 				}
 				echo "</td>";
